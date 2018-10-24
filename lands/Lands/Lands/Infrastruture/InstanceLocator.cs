a979fffc1476fa974feef94d6f1d77450ec74da7
Jose Ernesto Baltazar Torres <jose_torres04@outlook.com>
2018-10-23 23:12:38 -0500

    Lands

---
+++ lands/Lands/Lands/Infrastruture/InstanceLocator.cs
@@ -0,0 +1,22 @@
+﻿
+namespace Lands.Infrastruture
+{
+    using ViewModels;
+    public class InstanceLocator
+    {
+
+        #region Propertis
+        public MainViewModel Main
+        {
+            get; set;
+        }
+        #endregion
+
+        #region Constructos
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
---
+++ lands/Lands/Lands/ViewModels/MainViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Lands.ViewModels
+{
+	public class MainViewModel : ContentPage
+	{
+	
+	}
+}
